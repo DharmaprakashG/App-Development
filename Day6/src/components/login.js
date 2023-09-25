f8560cdcfba1cc9129149f491deda5b3548ac654
DP <120372081+DharmaprakashG@users.noreply.github.com>
2023-09-25 10:18:25 +0530

    Add files via upload

---
+++ Day6/src/components/login.js
@@ -0,0 +1,50 @@
+// Login.js
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import store from './store';
+import {setName} from './homereduser';
+// import Fitnesslogo from './Fitnesslogo.png'; 
+import './login.css'; 
+import homereduser from './homereduser';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const nav = useNavigate();
+
+  const handleLogin = () => {
+  nav("/dash")
+    console.log('Logging in with email:', email, 'and password:', password);
+  };
+
+  return (
+       <div className="container">  
+      <h2 className='mm'>Login</h2>     
+      <form>
+        <input className='mail'
+          type="email"
+          placeholder="Email"
+          // value={email}
+          onChange={(e) => {
+            store.dispatch(setName(e.target.value))
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleLogin}>   
+        Login 
+          {/* <Link to="/dash" className='but' >Login</Link> */}
+        </button>
+        <h5>Don't have an account?<Link to="/signup">Sign up</Link></h5>
+        
+      </form>
+    </div>
+  );
+};
+
+export default Login;
